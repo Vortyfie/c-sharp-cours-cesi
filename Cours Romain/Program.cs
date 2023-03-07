19172a8237bb5186358df228ac681a549da37c0e
Vortyfie <bastien.dasilva@icloud.com>
2023-03-07 09:18:59 +0100

    case

--- Cours Romain/Program.cs
+++ Cours Romain/Program.cs
@@ -16,22 +16,41 @@ class Program
 
         if (salaryIsInt && taxIsInt)
         {
-            int salarynet = Impots(salary, tax);
+            int salaryNet = Impots(salary, tax);
 
-            Console.WriteLine("Salaire Net : " + salarynet);
+            Console.WriteLine("Salaire Net : " + salaryNet);
 
-            if (salarynet < 1500)
+
+            // Affiche les différente message.
+            switch (salaryNet)
             {
-                Console.WriteLine("Tu est alternant pour toucher moins de 1500net par mois.");
+                case int n when n < 0:
+                    Console.WriteLine("Erreur : le salaire net ne peut pas être négatif.");
+                    break;
+                case int n when n < 1500:
+                    Console.WriteLine("Tu es alternant pour toucher moins de 1500 € par mois.");
+                    break;
+                default:
+                    Console.WriteLine("Tu n'es certainement pas alternant pour toucher plus de 1500 € par mois.");
+                    break;
             }
-            else
+            // Définit un tableau de chaînes de caractères
+            string[] mois = { "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre" };
+
+            int primeDecembre = 0;
+            if (mois[12] == "Décembre")
             {
-                Console.WriteLine("Tu est certernement pas alternant pour toucher plus de 1500€ par mois.");
+                primeDecembre = (int)(salary * 0.22);
+            }
+
+            for (int i = 0; i < mois.Length; i++)
+            {
+                Console.WriteLine(mois[i] + " : " + (i == 11 ? salaryNet + primeDecembre : salaryNet));
             }
         }
         else
         {
-            Console.WriteLine("Erreur : le salaire et la taxe doivent être des nombres entiers€.");
+            Console.WriteLine("Erreur : le salaire et la taxe doivent être des nombres entiers.");
         }
 
         Console.ReadLine();
@@ -41,8 +60,8 @@ class Program
     {
         int salary = a;
         double taxes = b / 100.0;
-        int salarytemp = (int)(a * taxes);
-        salary = salary - salarytemp;
+        int salaryTemp = (int)(a * taxes);
+        salary = salary - salaryTemp;
         return salary;
     }
 }
